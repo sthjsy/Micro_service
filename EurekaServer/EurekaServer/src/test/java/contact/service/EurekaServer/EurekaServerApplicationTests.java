c9340fc044cffdb817d5b85c81734f75609ea0b0
Jass ydv <sthjsy@gmail.com>
2022-12-24 22:37:01 +0530

    Add files via upload

---
+++ EurekaServer/EurekaServer/src/test/java/contact/service/EurekaServer/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package contact.service.EurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
